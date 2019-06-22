b6691a749c06678ff05195727e781229eb5aad14
ucmuller <ucmuller@gmail.com>
2019-06-22 19:45:46 +0900

    fix

--- plugins/vue2-google-maps.js
+++ plugins/vue2-google-maps.js
@@ -3,7 +3,7 @@ import * as VueGoogleMaps from '~/node_modules/vue2-google-maps/src/main';
 
 Vue.use(VueGoogleMaps, {
   load: {
-    key: 'AIzaSyAakjWRCf5vJKwi1O5eWOvzZjUpeoakO9c',
+    key: 'api-key',
     libraries: 'places',
   },
 });
